a75cb89a35de5482d310167787f33ff53c2b664a
zjbnyw <121949793@qq.com>
2024-06-15 23:13:18 +0800

    新增点击提示

--- background.js
+++ background.js
@@ -1,12 +1,29 @@
+// 创建上下文菜单项
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "sampleContextMenu",
+    title: "点击我！",
+    contexts: ["all"]
+  });
+});
 
-console.log(12312)
+// 监听上下文菜单项点击事件
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "sampleContextMenu") {
+    console.log(chrome.notifications)
+    chrome.notifications.create({
+      type: "basic",
+      iconUrl: './images/icon.png',
+      title: "通知",
+      message: "你点击了上下文菜单项！"
+    });
+  }
+});
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "fetchData") {
     console.log("Message received from popup.js:", request);
-
     // 处理逻辑并响应popup
-
     // 向content script发送消息
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       console.log(tabs, 111111111111)
@@ -16,8 +33,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       });
     });
   }
-  // Return true to indicate you wish to send a response asynchronously
-  return true;
+  console.log(request)
 });
 
 function getRandomHexColor() {
